a0dfd576d4d722f23cb067f034c148b372455fd5
Richard Chiu <richard.chiu87@gmail.com>
2023-03-15 21:47:20 -0400

    videos.js refactored

--- routes/videos.js
+++ routes/videos.js
@@ -5,37 +5,38 @@ const { v4: uuid } = require("uuid");
 
 const FILE_PATH = "./data/videos.json";
 
+// Refactored function to read videos from file
 const readVideos = () => {
-  const videosData = JSON.parse(fs.readFileSync(FILE_PATH));
-  return videosData;
+  return JSON.parse(fs.readFileSync(FILE_PATH));
 };
 
+// Refactored route handler for getting all videos
 router.get("/", (req, res) => {
-  const videosData = readVideos();
-  res.status(200).json(videosData);
+  res.status(200).json(readVideos());
 });
 
+// Refactored route handler for getting a single video
 router.get("/:id", (req, res) => {
   const id = req.params.id;
-  const videosData = readVideos();
-  const video = videosData.find((vid) => vid.id === id);
+  const video = readVideos().find((vid) => vid.id === id);
 
   if (!video) {
-    res.status(404).send("Video not found");
+    return res.status(404).send("Video not found");
   }
   res.send(video);
 });
 
+// Refactored route handler for creating a new video
 router.post("/", (req, res) => {
   const videoObj = req.body;
   const newVideo = {
     id: uuid(),
-    title: videoObj.title,
-    description: videoObj.description,
     timestamp: Date.now(),
+    channel: "coolestUser1",
     image: "http://localhost:8000/dataImages/image-default.jpeg",
     views: "0",
     likes: "0",
+    ...videoObj, // Use object spread to include additional properties from request body
   };
 
   const videosData = readVideos();
