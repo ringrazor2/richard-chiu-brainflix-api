321fbe277724db110d324ad17f84b2b488866dfe
Richard Chiu <richard.chiu87@gmail.com>
2023-03-15 21:49:20 -0400

    server.js refactored

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 const cors = require("cors");
 const app = express();
 const videosRoutes = require("./routes/videos");
-const bodyParser = require("body-parser");
 
 const PORT = process.env.PORT;
 const CLIENT_URL = process.env.CLIENT_URL;
@@ -14,13 +13,11 @@ app.use(
   })
 );
 app.use(express.json());
-app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("./public"));
 
 app.use("/videos", videosRoutes);
 
-app.use("/", videosRoutes);
 app.listen(PORT, () => {
   console.log(`🚀 Server listening on ${PORT}`);
 });
